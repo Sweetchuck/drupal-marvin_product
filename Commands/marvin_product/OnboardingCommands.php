1dae35faf53fb2560aeb9032d021cc03eda27138
Dávid Andor <david-andor@kozpontiagy.hu>
2018-12-17 13:27:09 +0100

    marvin:onboarding drush.local.yml and behat.local.yml

--- Commands/marvin_product/OnboardingCommands.php
+++ Commands/marvin_product/OnboardingCommands.php
@@ -9,11 +9,17 @@ use Drupal\marvin\Utils as MarvinUtils;
 use Drush\Commands\marvin\CommandsBase;
 use Robo\Collection\CollectionBuilder;
 use Robo\Contract\TaskInterface;
+use Stringy\StaticStringy;
+use Sweetchuck\Robo\Git\GitTaskLoader;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+use Webmozart\PathUtil\Path;
 
 class OnboardingCommands extends CommandsBase {
+
+  use GitTaskLoader;
 
   /**
    * @var \Symfony\Component\Filesystem\Filesystem
@@ -55,7 +61,7 @@ class OnboardingCommands extends CommandsBase {
       return $this
         ->collectionBuilder()
         ->addCode(function (): int {
-          $this->output()->writeln('Onboarding is skipped, because this project is still in template phase.');
+          $this->getLogger()->debug('Onboarding is skipped, because this project is still in template phase.');
 
           return 0;
         });
@@ -67,6 +73,8 @@ class OnboardingCommands extends CommandsBase {
       ->collectionBuilder()
       ->addTask($this->getTaskOnboardingCreateRequiredDirs($projectRoot, $drupalRoot, $siteDir))
       ->addCode($this->getTaskOnboardingSettingsLocalPhp($projectRoot, $drupalRoot, $siteDir))
+      ->addTask($this->getTaskOnboardingBehatLocalYml())
+      ->addCode($this->getTaskOnboardingDrushLocalYml($projectRoot))
       ->addCode($this->getTaskOnboardingHashSaltTxt($projectRoot, $siteDir));
   }
 
@@ -83,24 +91,31 @@ class OnboardingCommands extends CommandsBase {
 
   protected function getTaskOnboardingSettingsLocalPhp(string $projectRoot, string $drupalRoot, string $siteDir): \Closure {
     return function () use ($projectRoot, $drupalRoot, $siteDir) {
-      $src = $this->getExampleSettingsLocalPhp($projectRoot, $drupalRoot, $siteDir);
       $dst = "$projectRoot/$drupalRoot/sites/$siteDir/settings.local.php";
-      if (!$src || $this->fs->exists($dst)) {
+      if ($this->fs->exists($dst)) {
+        $this->getLogger()->debug('"settings.local.php" already exists');
+
         return 0;
       }
 
-      $this
+      $src = $this->getExampleSettingsLocalPhp($projectRoot, $drupalRoot, $siteDir);
+      if (!$src) {
+        $this->getLogger()->debug('There is no source for "settings.local.php"');
+
+        return 0;
+      }
+
+      $result = $this
         ->taskFilesystemStack()
         ->copy($src, $dst)
-        ->run()
-        ->stopOnFail();
+        ->run();
 
-      return 0;
+      return $result->wasSuccessful() ? 0 : 1;
     };
   }
 
   protected function getTaskOnboardingHashSaltTxt(string $projectRoot, string $siteDir): \Closure {
-    return function () use ($projectRoot, $siteDir) {
+    return function () use ($projectRoot, $siteDir): int {
       $fileName = "$projectRoot/sites/$siteDir/hash_salt.txt";
       if ($this->fs->exists($fileName)) {
         return 0;
@@ -111,6 +126,89 @@ class OnboardingCommands extends CommandsBase {
         ->text($this->generateHashSalt())
         ->run()
         ->stopOnFail();
+
+      return 0;
+    };
+  }
+
+  protected function getTaskOnboardingBehatLocalYml(): CollectionBuilder {
+    return $this
+      ->collectionBuilder()
+      ->addTask($this
+        ->taskGitListFiles()
+        ->setPaths(['behat.yml', '*/behat.yml'])
+      )
+      ->addTask($this
+        ->taskForEach()
+        ->deferTaskConfiguration('setIterable', 'files')
+        ->withBuilder(function (CollectionBuilder $builder, string $baseFileName) {
+          $builder->addCode($this->getTaskOnboardingBehatLocalYmlSingle($baseFileName));
+        })
+      );
+  }
+
+  protected function getTaskOnboardingBehatLocalYmlSingle(string $baseFileName): \Closure {
+    return function () use ($baseFileName): int {
+      $behatDir = Path::getDirectory($baseFileName);
+      $exampleFileName = "$behatDir/behat.local.example.yml";
+      $localFileName = "$behatDir/behat.local.yml";
+
+      if ($this->fs->exists($exampleFileName)) {
+        $this->fs->copy($exampleFileName, $localFileName);
+
+        return 0;
+      }
+
+      $this->fs->dumpFile($localFileName, '{}');
+
+      return 0;
+    };
+  }
+
+  protected function getTaskOnboardingDrushLocalYml(string $projectRoot): \Closure {
+    return function () use ($projectRoot): int {
+      $localFileName = "$projectRoot/drush/drush.local.yml";
+      if ($this->fs->exists($localFileName)) {
+        $this->getLogger()->debug(
+          'File "<info>{fileName}</info>" already exists',
+          ['fileName' => $localFileName]
+        );
+
+        return 0;
+      }
+
+      $exampleFileName = "$projectRoot/drush/drush.local.example.yml";
+      if ($this->fs->exists($exampleFileName)) {
+        $this->fs->copy($exampleFileName, $localFileName);
+
+        return 0;
+      }
+
+      $baseFileName = "$projectRoot/drush/drush.yml";
+      if (!$this->fs->exists($baseFileName)) {
+        return 0;
+      }
+
+      $uri = $this->getLocalUri();
+      $baseContent = Yaml::parseFile($baseFileName);
+      if (isset($baseContent['command']['options']['uri'])
+        && $baseContent['command']['options']['uri'] === $uri
+      ) {
+        return 0;
+      }
+
+      $localContent = [
+        'command' => [
+          'options' => [
+            'uri' => $uri,
+          ],
+        ],
+      ];
+
+      $this->fs->dumpFile(
+        $localFileName,
+        Yaml::dump($localContent, 42, 2)
+      );
 
       return 0;
     };
@@ -133,6 +231,21 @@ class OnboardingCommands extends CommandsBase {
 
   protected function generateHashSalt(): string {
     return uniqid('', TRUE);
+  }
+
+  protected function fileGetContents(string $fileName): string {
+    $content = file_get_contents($fileName);
+    if ($content === FALSE) {
+      throw new \Exception('@todo');
+    }
+
+    return $content;
+  }
+
+  protected function getLocalUri(): string {
+    $composerInfo = $this->getComposerInfo();
+
+    return sprintf('http://%s.localhost', StaticStringy::dasherize($composerInfo->packageName));
   }
 
 }
