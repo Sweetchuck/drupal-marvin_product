250c5842a66126a464ac3e9726c3adefb17acb06
Dávid Andor <david-andor@kozpontiagy.hu>
2022-02-07 21:09:02 +0100

    PHPCS installed_paths setter in favor of dealerdirect/phpcodesniffer-composer-installer

--- Commands/marvin_product/PhpcsCommands.php
+++ Commands/marvin_product/PhpcsCommands.php
@@ -6,35 +6,8 @@ namespace Drush\Commands\marvin_product;
 
 use Drush\Commands\marvin\PhpcsCommandsBase;
 use Robo\Collection\CollectionBuilder;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class PhpcsCommands extends PhpcsCommandsBase {
-
-  /**
-   * @hook on-event marvin:composer-scripts:post-install-cmd
-   *
-   * @todo Remove this. dealerdirect/phpcodesniffer-composer-installer.
-   */
-  public function onEventComposerScriptsPostInstallCmd(InputInterface $input, OutputInterface $output, string $projectRoot): array {
-    $tasks = [];
-
-    if ($input->getOption('dev-mode')) {
-      $tasks['marvin.phpcs.config.installed_paths'] = [
-        'weight' => 100,
-        'task' => $this->getTaskPhpcsConfigSetInstalledPaths($projectRoot),
-      ];
-    }
-
-    return $tasks;
-  }
-
-  /**
-   * @hook on-event marvin:composer-scripts:post-update-cmd
-   */
-  public function onEventComposerScriptsPostUpdateCmd(InputInterface $input, OutputInterface $output, string $projectRoot): array {
-    return $this->onEventComposerScriptsPostInstallCmd($input, $output, $projectRoot);
-  }
 
   /**
    * @hook pre-command marvin:git-hook:pre-commit
